d4ca9ba5012dd913ff6880b1bd6875afb89aa95c
Patrick Drechsler <patrick@pdrechsler.de>
2010-05-11 21:03:06 +0200

    chapter 7 (before exercise): added gravatar plugin; User model with passwords, and user show page.

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
   def logo
     logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
+
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{h(@title)}"
+    end
+  end
+
+
 end
--- sample_app/config/environments/test.rb
+++ sample_app/config/environments/test.rb
@@ -1,5 +1,8 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
+## PD 2010-05-10 Added Factory Girl
+config.gem 'factory_girl'
+
 # The test environment is used exclusively to run your application's
 # test suite.  You never need to work with it otherwise.  Remember that
 # your test database is "scratch space" for the test suite and is wiped
