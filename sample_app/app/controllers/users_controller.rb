1e2edcebe5ed44a5a91d8455b7ee2825205ebab5
Patrick Drechsler <patrick@pdrechsler.de>
2010-05-12 21:18:50 +0200

    Chapter 8.2 Invalid sign up

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,4 +10,13 @@ class UsersController < ApplicationController
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # TODO Handle a successful save
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
--- sample_app/spec/controllers/users_controller_spec.rb
+++ sample_app/spec/controllers/users_controller_spec.rb
@@ -52,4 +52,31 @@ describe UsersController do
 
   end
 
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = {:name => "", :email => "", :password => "",
+          :password_confirmation => "" }
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /sign up/i)
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+    end
+  end
+
 end
+
+
