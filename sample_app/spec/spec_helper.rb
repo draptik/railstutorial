e18cec77556b539f4cc38708f50920863f2bfc7c
Patrick Drechsler <patrick@pdrechsler.de>
2010-05-14 17:09:56 +0200

    Chapter 8.4 and 8.5: User signup complete

--- sample_app/spec/spec_helper.rb
+++ sample_app/spec/spec_helper.rb
@@ -5,6 +5,12 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environ
 require 'spec/autorun'
 require 'spec/rails'
 
+## PD 2010-05-12 WebRats
+require 'webrat'
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
 
