755b08dff52df2429e0b92d926012fcd69e69361
Patrick Drechsler <patrick@pdrechsler.de>
2010-05-31 21:32:25 +0200

    added some comments.

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -16,7 +16,13 @@ module ApplicationHelper
 
 
   def warn_if_word_count_exceeded(field_id, update_id, maximum_number_of_words, options = {})
+    ##
+    ## URL for wordcount: http://stufftohelpyouout.blogspot.com/2009/09/rubyrails-and-javascript-word-count.html
+    ##
     # function = "var words = []; $F('#{field_id}').scan(/\\w+/, function(match){ words.push(match[0]) }); if (words.length > #{maximum_number_of_words}) { $('#{update_id}').innerHTML = 'Your entry contains ' + words.length + ' words. Please limit your answer to #{maximum_number_of_words} words.' } else { $('#{update_id}').innerHTML = '' };" 
+    ##
+    ## URL for basic char count: http://www.swards.net/2009/05/character-count-textarea-in-ruby-on.html
+    ##
     function = "$('#{update_id}').innerHTML = $F('#{field_id}').length;"
     out = javascript_tag(function)
     out += observe_field(field_id, options.merge(:function => function))
