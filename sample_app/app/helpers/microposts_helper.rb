f975ed4be146e00ab8a3c670221ec4f7cdf36993
Patrick Drechsler <patrick@pdrechsler.de>
2010-06-17 00:46:23 +0200

    Exercise 11.5.9 Wrap method to split long words

--- sample_app/app/helpers/microposts_helper.rb
+++ sample_app/app/helpers/microposts_helper.rb
@@ -8,6 +8,24 @@ module MicropostsHelper
   ## includes a URL (such as “I'm reading an awesome Rails book at
   ## http://www.railstutorial.org/!”).
   def display(content)
-    auto_link(sanitize(content))
+    auto_link(sanitize(wrap(content)))
+  end
+
+
+  ## Exercise 11.5.9 Very long words currently break our layout, as
+  ## shown in Figure 11.17. Fix this problem using the wrap helper
+  ## defined in Listing 11.43
+  ##
+  ## Listing 11.43
+  def wrap(content)
+    content.split.map{ |s| wrap_long_string(s) }.join(' ')
+  end
+
+  private
+
+  def wrap_long_string(text, max_width = 30)
+    zero_width_space = "&#8203;"
+    regex = /.{1,#{max_width}}/
+    (text.length < max_width) ? text : text.scan(regex).join(zero_width_space)
   end
 end
