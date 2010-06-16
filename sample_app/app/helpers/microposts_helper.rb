5d7ec0720d66f65557fd2a5f071185b0c323c276
Patrick Drechsler <patrick@pdrechsler.de>
2010-06-17 00:13:06 +0200

    Exercise 11.5.7 Added display helper including auto_link and sanitize methods; Forgot to commit the helper method

--- sample_app/app/helpers/microposts_helper.rb
+++ sample_app/app/helpers/microposts_helper.rb
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
 module MicropostsHelper
 
+  ## Exercise 11.5.7 Define a display helper using the sanitize and
+  ## auto_link methods, as shown in Listing 11.42, and use it to
+  ## replace the h method in the micropost partial (Listing
+  ## 11.17). Examine the results for a micropost whose content
+  ## includes a URL (such as “I'm reading an awesome Rails book at
+  ## http://www.railstutorial.org/!”).
   def display(content)
     auto_link(sanitize(content))
   end
